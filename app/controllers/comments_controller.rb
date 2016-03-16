974e521c40c499f62ab93cd0b62e23a70bc974db
Christine Movius <christine.movius@gmail.com>
2016-03-16 17:25:07 -0400

    User can add comments that are nested under a post.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,8 +4,9 @@ class CommentsController < ApplicationController
   end
 
   def create
-    @comment = Comment.create! comment_params
-    redirect_to "/comments"
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.create! comment_params
+    redirect_to post_path(@post)
   end
 
   def new
